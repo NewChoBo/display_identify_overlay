9df9796104795bf3a26e5d55bfe548a9269f0456
NewChoBo <whwjdrb1009@gmail.com>
2025-08-14 00:36:36 +0900

    buid(ci): add github workflow

--- lib/display_identify_overlay.dart
+++ lib/display_identify_overlay.dart
@@ -1,6 +1,4 @@
-export 'src/core/platform_detector.dart';
-export 'src/display_identify_overlay.dart';
+export 'src/exceptions/display_identify_exception.dart';
 export 'src/models/monitor_info.dart';
 export 'src/models/overlay_options.dart';
 export 'src/models/overlay_style.dart';
-export 'src/exceptions/display_identify_exception.dart';
